01ef52ace7150ceee26c57870a7188d58558092e
karik87 <109695014+karik87@users.noreply.github.com>
2024-12-13 11:23:15 +0100

    Add files via upload

---
+++ myproject/myapp/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+
+
+# deklaracja statycznej listy wyboru do wykorzystania w klasie modelu
+MONTHS = models.IntegerChoices('Miesiace', 'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
+
+PLEC = models.IntegerChoices("Płeć", "kobieta mężczyzna inne")
+
+SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=60)
+    country = models.CharField(max_length=2)
+    website = models.URLField(null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Person(models.Model):
+
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES, default=SHIRT_SIZES[0][0])
+    month_added = models.IntegerField(choices=MONTHS.choices, default=MONTHS.choices[0][0])
+    team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL, verbose_name="Choose yout team")
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+# skrót Shift + Alt + strzałka góra/dół
+
+class Stanowisko(models.Model):
+    nazwa = models.CharField(max_length=50)
+    opis = models.TextField(null=True, blank=True)
+    class Meta:
+        verbose_name_plural = "Stanowiska"
+
+class Osoba(models.Model):
+    imie = models.CharField(max_length=60, blank=False)
+    nazwisko = models.CharField(max_length=60, blank=False)
+    plec = models.IntegerField(choices=PLEC.choices, default=PLEC.choices[0][0])
+    stanowisko = models.ForeignKey(Stanowisko, null=True, blank=True, on_delete=models.SET_NULL)
+    data_dodania = models.DateField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = "Osoba"
+
+class Meta:
+    verbose_name_plural = "Osoby"
---
+++ myproject/myapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+from django.http import Http404, HttpResponse
+import datetime
+
+
+def welcome_view(request):
+    now = datetime.datetime.now()
+    html = f"""
+        <html><body>
+        Witaj użytkowniku! </br>
+        Aktualna data i czas na serwerze: {now}.
+        </body></html>"""
+    return HttpResponse(html)
+
+from .models import Person
+
+def person_list(request):
+    persons = Person.objects.all()
+
+    return render(request,
+                  "myapp/person/list.html",
+                  {'persons': persons})
+
+def person_detail(request, id):
+    # pobieramy konkretny obiekt Person
+    try:
+        person = Person.objects.get(id=id)
+    except Person.DoesNotExist:
+        raise Http404("Obiekt Person o podanym id nie istnieje")
+
+    return render(request,
+                  "myapp/person/detail.html",
+                  {'person': person})
